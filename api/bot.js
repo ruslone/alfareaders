453bb0d748c1c54a8d9c7ab56b5b8038173e9414
Petr Kozachenko <pkozachenko@alfabank.ru>
2026-01-10 12:35:27 +0300

    tg bot

---
+++ api/bot.js
@@ -0,0 +1,31 @@
+import TelegramBot from "node-telegram-bot-api";
+
+const bot = new TelegramBot(process.env.BOT_TOKEN);
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(200).send("OK");
+  }
+
+  const update = req.body;
+
+  if (update.message?.text === "/start") {
+    await bot.sendMessage(
+      update.message.chat.id,
+      "Нажми меня",
+      {
+        reply_markup: {
+          keyboard: [[{
+            text: "Открыть приложение",
+            web_app: {
+              url: ""
+            }
+          }]],
+        resize_keyboard: true
+        }
+      }
+    );
+  }
+
+  res.status(200).send("OK");
+}
